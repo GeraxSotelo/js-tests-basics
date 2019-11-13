daa49ea968e3b5059c9d3b116ea0c31d871c0d9e
Gerax Sotelo <gsotelo23@gmail.com>
2019-11-12 21:26:01 -0700

    complete bonuses section

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,13 @@
 let str = "team number 2"
 
 function camelCase(str) {
+  let strArr = str.split(" ");
+  let camelCaseWord = strArr[0];
 
+  for (let i = 1; i < strArr.length; i++) {
+    let wordArr = strArr[i].split("");
+    wordArr[0] = wordArr[0].toUpperCase();
+    camelCaseWord += wordArr.join('');
+  }
+  return camelCaseWord;
 }
